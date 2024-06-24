b0fd97e23a4ec589bec7bfbd4fb7346d5b1e70a7
KD <kdkahlo@gmail.com>
2024-06-24 17:35:03 +0200

    staircase hackerrank challenge started

---
+++ staircase.js
@@ -0,0 +1,3 @@
+//I need to use the "#" for the steps in the staircase
+//I need to be able to enter a number for the n parameter and create steps with that number
+//To to this I can use a while the number is less than 
