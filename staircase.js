e581e741e4d6f0c565a5201cc06498b2d9a8dff4
KD <kdkahlo@gmail.com>
2024-07-05 14:24:13 +0200

    staircase function completed

--- staircase.js
+++ staircase.js
@@ -1,21 +1,15 @@
 function staircase(n) {
- // Loop from 1 to n, for each iteration print n-1
+ // Loop from i to n, for each iteration print n-i
  // number of hashes in each line (representing the staircase)
-    // for (let i = 1; i <= n; i++) {
-    //     // Print spaces before hashes
-    //     for (let j = 1; j <= n - i; j++) {
-    //         console.log(" ");
-    //     }
-    //     // Print hashes
-    //     for (let k = 1; k <= i; k++) {
-    //         console.log("#");
-    //     }
-    //     console.log("\n");
-    // }
-    for (let hash = "#"; hash.length < n; hash++){
-        console.log(hash);
-      
+ 
+    for (let i = 1; i <= n; i++) {
+        //calculate the number of spaces and hashes
+        let space = ' '.repeat(n- i);
+        let hashes = '#'.repeat(i);
+        //print the line
+        console.log(space + hashes)
+
     }
-    
+   
 }
-console.log(staircase(5))
+staircase(6) 
