51b941b73467d382f01c43b2b2d76afa2e758f9b
KD <kdkahlo@gmail.com>
2024-07-05 15:05:16 +0200

    birthdayCakeCandles function completed not tested

---
+++ birthdayCakeCandles.js
@@ -0,0 +1,19 @@
+function birthdayCakeCandles (candles) {
+    //the maximum height on the candles are 4.
+    //the person will only be able to blow out the tallest of the candles.
+   
+    //step 1:
+    for (let index = 0; index < candles.length; index++) {
+        if (candles[index] > maxHeight) {
+            maxHeight = candles[index];
+        } 
+    }
+    let tallestCandles = 0;
+    for (let i = 0; i < candles.length; i++) {
+        if (candles[i] > maxHeight) {
+            tallestCandles++;
+        }
+        
+    }
+return tallestCandles;
+}
